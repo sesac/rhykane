76d7a13d9ee4f2c0885bb6f0fe304bd4e5ecc07f
Ian MacDonald <imacdonald@rumblefish.com>
2023-01-13 13:52:35 -0800

    CIR-371-s3-put
    Need s3 get and put integrations for streaming.
    
    Get from s3 yielding chunks to a pipe.
    Stream from pipe to s3 object.

---
+++ lib/rhykane/s3/put.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require_relative 'get'
+
+module Rhykane
+  module S3
+    class Put < Get
+      class << self
+        def call(*deps, **args)
+          *rest, io = *deps
+
+          new(*rest, **args).(io)
+        end
+      end
+
+      def call(input_io)
+        object.upload_stream do |stream|
+          IO.copy_stream(input_io, stream)
+        end
+      end
+    end
+  end
+end
---
+++ spec/rhykane/s3/put_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require './lib/rhykane/s3/put'
+
+describe Rhykane::S3::Put do
+  describe '.call' do
+    it 'streams file in s3 to a pipe and yields the pipe' do
+      res      = stub_s3_resource
+      key_path = s3_path(bucket = 'foo', key = 'streamable.txt').tap(&:delete)
+      io       = StringIO.new.tap { |s|
+        s.puts(*Array.new(1024) { 'a' * 1024 })
+        s.rewind
+      }
+
+      described_class.(res, io, bucket:, key:)
+
+      expect(key_path.read).to eq io.tap(&:rewind).read
+    end
+  end
+end
