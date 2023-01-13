97ef4d1ce737fe7c9e5606eaa36d7fa7963bb15f
Ian MacDonald <imacdonald@rumblefish.com>
2023-01-13 13:55:50 -0800

    CIR-371-s3-put spec idempotency

--- spec/rhykane/s3/put_spec.rb
+++ spec/rhykane/s3/put_spec.rb
@@ -6,7 +6,7 @@ describe Rhykane::S3::Put do
   describe '.call' do
     it 'streams file in s3 to a pipe and yields the pipe' do
       res      = stub_s3_resource
-      key_path = s3_path(bucket = 'foo', key = 'streamable.txt').tap(&:delete)
+      key_path = s3_path(bucket = 'foo', key = 'streamable.txt').tap { |p| p.delete if p.exist? }
       io       = StringIO.new.tap { |s|
         s.puts(*Array.new(1024) { 'a' * 1024 })
         s.rewind
