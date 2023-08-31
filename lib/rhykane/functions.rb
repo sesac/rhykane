67b956656ec5e6c64ebd8923502a03f277dbd007
Johnny Bowman <jbowman@harryfox.com>
2023-08-31 15:17:26 -0600

    add handling for quarterly formats in parse_period function

--- lib/rhykane/functions.rb
+++ lib/rhykane/functions.rb
@@ -5,11 +5,22 @@ module Functions
     JSON.dump(value) unless value.nil?
   end
 
-  def parse_period(value, type)
+  def parse_period(value, args)
     return if value.nil?
 
-    date = Date.strptime(value.slice(2..-1), '%m%y')
+    type, date_format, quarter_type = args.values_at(:type, :date_format, :quarter_type)
+    date = Date.strptime(value, date_format)
     day = type == :start ? 1 : -1
-    Date.new(date.year, date.month, day).to_s
+    return Date.new(date.year, date.month, day).to_s if !quarter_type
+    quarterly_format(quarter_type, type, date, day)
+  end
+
+  def quarterly_format(quarter_type, type, date, day)
+    if quarter_type == :ordinal
+      month = type == :start ? date.month * 3 - 2 : date.month * 3
+    else
+      month = type == :start ? date.month - 2 : date.month
+    end
+    Date.new(date.year, month, day).to_s
   end
 end
--- spec/rhykane/functions_spec.rb
+++ spec/rhykane/functions_spec.rb
@@ -12,14 +12,54 @@ describe Functions do
     expect { JSON.parse(result) }.to_not raise_error
   end
 
-  it 'formats period start & end' do
+  it 'formats monthly period start & end' do
     value = '011222'
-    result = dummy_class.parse_period(value, :start)
+    start_args = { type: :start, date_format: '01%m%y' }
+    result = dummy_class.parse_period(value, start_args)
 
     expect(Date.parse(result).day).to eq(1)
+    expect(Date.parse(result).month).to eq(12)
+    expect(Date.parse(result).year).to eq(2022)
 
-    result = dummy_class.parse_period(value, :end)
+    end_args = { type: :end, date_format: '01%m%y' }
+    result = dummy_class.parse_period(value, end_args)
 
     expect(Date.parse(result).day).to eq(31)
+    expect(Date.parse(result).month).to eq(12)
+    expect(Date.parse(result).year).to eq(2022)
+  end
+
+  it 'formats ordinal quarterly period start & end' do
+    value = 'Q4 2020'
+    start_args = { type: :start, date_format: 'Q%m %Y', quarter_type: :ordinal }
+    result = dummy_class.parse_period(value, start_args)
+
+    expect(Date.parse(result).day).to eq(1)
+    expect(Date.parse(result).month).to eq(10)
+    expect(Date.parse(result).year).to eq(2020)
+
+    end_args = { type: :end, date_format: 'Q%m %Y', quarter_type: :ordinal }
+    result = dummy_class.parse_period(value, end_args)
+
+    expect(Date.parse(result).day).to eq(31)
+    expect(Date.parse(result).month).to eq(12)
+    expect(Date.parse(result).year).to eq(2020)
+  end
+
+  it 'formats numeric quarterly period start & end' do
+    value = '202209'
+    start_args = { type: :start, date_format: '%Y%m', quarter_type: :numeric }
+    result = dummy_class.parse_period(value, start_args)
+
+    expect(Date.parse(result).day).to eq(1)
+    expect(Date.parse(result).month).to eq(7)
+    expect(Date.parse(result).year).to eq(2022)
+
+    end_args = { type: :end, date_format: '%Y%m', quarter_type: :numeric }
+    result = dummy_class.parse_period(value, end_args)
+
+    expect(Date.parse(result).day).to eq(30)
+    expect(Date.parse(result).month).to eq(9)
+    expect(Date.parse(result).year).to eq(2022)
   end
 end
