933daa8d9458520789570149bfc813b789ae6245
Anna Royer <anna.royer@colorado.edu>
2023-09-13 20:43:14 -0700

    remove extra code

--- lib/rhykane/s3/get.rb
+++ lib/rhykane/s3/get.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 require 'aws-sdk-s3'
-# require 'open3'
 require 'zip'
 
 class Rhykane
   module S3
-
     class Get
       DECOMPRESSION_STRATEGIES = Hash.new('stream').merge( zip: 'unzip' ).freeze
 
@@ -40,9 +38,7 @@ class Rhykane
 
       def get_thread(pipe)
         Thread.new {
-          read do |chunk|
-            pipe << chunk
-          end
+          read do |chunk| pipe << chunk end
           pipe.close
         }
       end
@@ -56,34 +52,13 @@ class Rhykane
 
       class Unzip < Get
 
-        def initialize(client = Aws::S3::Resource.new, bucket:, key:, type:, **)
-          super
-
-          @key_path   = Pathname(key)
-          @tmp_root   = Pathname(ENV.fetch('TMP_ROOT', '/mnt/tmp'))
-          @local_path = @tmp_root.join(key_path)
-          @type       = type
-        end
-
-        def call
-          local_path.dirname.mkpath
-          object.download_file(local_path)
-          super
-        ensure
-          local_path.delete if local_path.exist?
-        end
-
         private
 
-        attr_reader :local_path, :key_path, :type
-
         def read
-          ::Zip::File.open(local_path) do |zip_file|
-            zip_file.map do |entry|
-              zip_file.get_input_stream(entry)
+          ::Zip::File.open_buffer(object.get.body) do |zip_file|
+            zip_file.each do |entry|
+              yield zip_file.read(entry)
             end
-          end.flatten.each do |io|
-            yield io.read
           end
         end
       end
